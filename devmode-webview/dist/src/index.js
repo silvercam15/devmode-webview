f23ba02a88e100d30020fccd60197d83ff2a3f2e
Stephen Gontzes <stephen.gontzes@lightstep.com>
2022-07-26 15:47:01 -0400

    removed git cache of dist folder

--- devmode-webview/dist/src/index.js
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
-ReactDOM.render(React.createElement("div", null,
-    React.createElement(App, null)), document.getElementById("root"));
